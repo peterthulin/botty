fe02dfa755269400c631704f8e4a84f1f33ba752
Peter Thulin <peter.thulin@aith.se>
2020-06-26 09:48:25 +0200

    Change to BCM pin structure

--- motor.py
+++ motor.py
@@ -1,21 +1,20 @@
 import time
 import RPi.GPIO as GPIO
 
-PIN_PWMA = 7
-PIN_AIN1 = 12
-PIN_AIN2 = 11
+PIN_PWMA = 4
+PIN_AIN1 = 18
+PIN_AIN2 = 17
 
-PIN_PWMB = 15
-PIN_BIN1 = 13
-PIN_BIN2 = 16
+PIN_PWMB = 22
+PIN_BIN1 = 27
+PIN_BIN2 = 23
 
 ALL_PINS = [
 	PIN_PWMA, PIN_AIN1, PIN_AIN2,
 	PIN_PWMB, PIN_BIN1, PIN_BIN2]
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-# GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
 
 # set up GPIO pins
 GPIO.setup(PIN_PWMA, GPIO.OUT)
