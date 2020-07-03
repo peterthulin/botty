0d44b95de84aaae753e15dd0704fe3b629a8f3de
Peter Thulin <peter.thulin@aith.se>
2020-07-03 14:55:22 +0200

    Add pin config and change api

--- motor.py
+++ motor.py
@@ -4,60 +4,56 @@ import RPi.GPIO as GPIO
 
 class MotorDriver_TB6612():
 
-    def __init__(self,):
-        # TODO: let pins be input parameters
-        self.PIN_PWMA = 12
-        self.PIN_AIN1 = 20
-        self.PIN_AIN2 = 16
-        self.PIN_PWMB = 13
-        self.PIN_BIN1 = 5
-        self.PIN_BIN2 = 6
+    def __init__(self, pin_config, a_is_left=True):
+        if a_is_left:
+            self.left_pwm = pin_config["PWMA"]
+            self.left_in1 = pin_config["AIN1"]
+            self.left_in2 = pin_config["AIN2"]
+            self.right_pwm = pin_config["PWMB"]
+            self.right_in1 = pin_config["BIN1"]
+            self.right_in2 = pin_config["BIN2"]
+        else:
+            self.left_pwm = pin_config["PWMB"]
+            self.left_in1 = pin_config["BIN1"]
+            self.left_in2 = pin_config["BIN2"]
+            self.right_pwm = pin_config["PWMA"]
+            self.right_in1 = pin_config["AIN1"]
+            self.right_in2 = pin_config["AIN2"]
+
         self.all_pins = [
-            self.PIN_PWMA, self.PIN_AIN1, self.PIN_AIN2,
-            self.PIN_PWMB, self.PIN_BIN1, self.PIN_BIN2
+            self.left_pwm, self.left_in1, self.left_in2,
+            self.right_pwm, self.right_in1, self.right_in2
         ]
 
         self._init_gpio()
 
     def _init_gpio(self):
-        GPIO.setup(self.PIN_PWMA, GPIO.OUT)
-        GPIO.setup(self.PIN_AIN1, GPIO.OUT)
-        GPIO.setup(self.PIN_AIN2, GPIO.OUT)
-        GPIO.setup(self.PIN_PWMB, GPIO.OUT)
-        GPIO.setup(self.PIN_BIN1, GPIO.OUT)
-        GPIO.setup(self.PIN_BIN2, GPIO.OUT)
+        GPIO.setup(self.left_pwm, GPIO.OUT)
+        GPIO.setup(self.left_in1, GPIO.OUT)
+        GPIO.setup(self.left_in2, GPIO.OUT)
+        GPIO.setup(self.right_pwm, GPIO.OUT)
+        GPIO.setup(self.right_in1, GPIO.OUT)
+        GPIO.setup(self.right_in2, GPIO.OUT)
 
-    def forward(self):
-        GPIO.output(self.PIN_AIN1, GPIO.HIGH)
-        GPIO.output(self.PIN_AIN2, GPIO.LOW)
-        GPIO.output(self.PIN_PWMA, GPIO.HIGH)
-        GPIO.output(self.PIN_BIN1, GPIO.HIGH)
-        GPIO.output(self.PIN_BIN2, GPIO.LOW)
-        GPIO.output(self.PIN_PWMB, GPIO.HIGH)
+    def left_forward(self):
+        GPIO.output(self.left_in1, GPIO.HIGH)
+        GPIO.output(self.left_in2, GPIO.LOW)
+        GPIO.output(self.left_pwm, GPIO.HIGH)
 
-    def backward(self):
-        GPIO.output(self.PIN_AIN1, GPIO.LOW)
-        GPIO.output(self.PIN_AIN2, GPIO.HIGH)
-        GPIO.output(self.PIN_PWMA, GPIO.HIGH)
-        GPIO.output(self.PIN_BIN1, GPIO.LOW)
-        GPIO.output(self.PIN_BIN2, GPIO.HIGH)
-        GPIO.output(self.PIN_PWMB, GPIO.HIGH)
+    def right_forward(self):
+        GPIO.output(self.right_in1, GPIO.HIGH)
+        GPIO.output(self.right_in2, GPIO.LOW)
+        GPIO.output(self.right_pwm, GPIO.HIGH)
 
-    def rotate_left(self):
-        GPIO.output(self.PIN_AIN1, GPIO.LOW)
-        GPIO.output(self.PIN_AIN2, GPIO.HIGH)
-        GPIO.output(self.PIN_PWMA, GPIO.HIGH)
-        GPIO.output(self.PIN_BIN1, GPIO.HIGH)
-        GPIO.output(self.PIN_BIN2, GPIO.LOW)
-        GPIO.output(self.PIN_PWMB, GPIO.HIGH)
+    def left_backward(self):
+        GPIO.output(self.left_in1, GPIO.LOW)
+        GPIO.output(self.left_in2, GPIO.HIGH)
+        GPIO.output(self.left_pwm, GPIO.HIGH)
 
-    def rotate_right(self):
-        GPIO.output(self.PIN_AIN1, GPIO.HIGH)
-        GPIO.output(self.PIN_AIN2, GPIO.LOW)
-        GPIO.output(self.PIN_PWMA, GPIO.HIGH)
-        GPIO.output(self.PIN_BIN1, GPIO.LOW)
-        GPIO.output(self.PIN_BIN2, GPIO.HIGH)
-        GPIO.output(self.PIN_PWMB, GPIO.HIGH)
+    def right_backward(self):
+        GPIO.output(self.right_in1, GPIO.LOW)
+        GPIO.output(self.right_in2, GPIO.HIGH)
+        GPIO.output(self.right_pwm, GPIO.HIGH)
 
     def stop(self):
         for pin in self.all_pins:
@@ -66,22 +62,34 @@ class MotorDriver_TB6612():
 
 def main():
     """ Simple main loop for testing """
+
+    pin_config = {
+        "PWMA": 12,
+        "AIN1": 20,
+        "AIN2": 16,
+        "PWMB": 13,
+        "BIN1": 5,
+        "BIN2": 6
+    }
+
     GPIO.setmode(GPIO.BCM)
-    motor_driver = MotorDriver_TB6612()
+    motor_driver = MotorDriver_TB6612(pin_config)
 
     # Test all motor functions for 1 sec
-    motor_driver.forward()
+    motor_driver.left_forward()
     time.sleep(1.0)
+    motor_driver.stop()
 
-    motor_driver.backward()
+    motor_driver.right_forward()
     time.sleep(1.0)
+    motor_driver.stop()
 
-    motor_driver.rotate_left()
+    motor_driver.left_backward()
     time.sleep(1.0)
+    motor_driver.stop()
 
-    motor_driver.rotate_right()
+    motor_driver.right_backward()
     time.sleep(1.0)
-
     motor_driver.stop()
 
 
