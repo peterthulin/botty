277d8d2826fd1440cf7bd5d89557a69297314494
Peter Thulin <peter.thulin@aith.se>
2020-07-03 15:35:33 +0200

    Add support for pwm

--- motor.py
+++ motor.py
@@ -1,4 +1,6 @@
+import argparse
 import time
+
 import RPi.GPIO as GPIO
 
 
@@ -35,33 +37,49 @@ class MotorDriver_TB6612():
         GPIO.setup(self.right_in1, GPIO.OUT)
         GPIO.setup(self.right_in2, GPIO.OUT)
 
-    def left_forward(self):
+        self.left_pwm_instance = GPIO.PWM(self.left_pwm, 0.0)
+        self.right_pwm_instance = GPIO.PWM(self.right_pwm, 0.0)
+
+    def left_forward(self, dc=100.0, freq=10.0):
         GPIO.output(self.left_in1, GPIO.HIGH)
         GPIO.output(self.left_in2, GPIO.LOW)
-        GPIO.output(self.left_pwm, GPIO.HIGH)
+        self.set_left_pwm(dc, freq)
 
-    def right_forward(self):
+    def right_forward(self, dc=100.0, freq=10.0):
         GPIO.output(self.right_in1, GPIO.HIGH)
         GPIO.output(self.right_in2, GPIO.LOW)
-        GPIO.output(self.right_pwm, GPIO.HIGH)
+        self.set_left_pwm(dc, freq)
 
-    def left_backward(self):
+    def left_backward(self, dc=100.0, freq=10.0):
         GPIO.output(self.left_in1, GPIO.LOW)
         GPIO.output(self.left_in2, GPIO.HIGH)
-        GPIO.output(self.left_pwm, GPIO.HIGH)
+        self.set_right_pwm(dc, freq)
 
-    def right_backward(self):
+    def right_backward(self, dc=100.0, freq=10.0):
         GPIO.output(self.right_in1, GPIO.LOW)
         GPIO.output(self.right_in2, GPIO.HIGH)
-        GPIO.output(self.right_pwm, GPIO.HIGH)
+        self.set_right_pwm(dc, freq)
 
     def stop(self):
         for pin in self.all_pins:
             GPIO.output(pin, GPIO.LOW)
+        self.left_pwm_instance.ChangeDutyCycle(0.0)
+        self.right_pwm_instance.ChangeDutyCycle(0.0)
+
+    def set_left_pwm(self, dc, freq):
+        self.left_pwm_instance.ChangeFrequency(freq)
+        self.left_pwm_instance.ChangeDutyCycle(dc)
+
+    def set_right_pwm(self, dc, freq):
+        self.right_pwm_instance.ChangeFrequency(freq)
+        self.right_pwm_instance.ChangeDutyCycle(dc)
 
 
 def main():
     """ Simple main loop for testing """
+    args = parse_args()
+    dc = args.pwm_dc
+    freq = args.pwm_freq
 
     pin_config = {
         "PWMA": 12,
@@ -76,21 +94,32 @@ def main():
     motor_driver = MotorDriver_TB6612(pin_config)
 
     # Test all motor functions for 1 sec
-    motor_driver.left_forward()
+    motor_driver.left_forward(dc, freq)
     time.sleep(1.0)
     motor_driver.stop()
 
-    motor_driver.right_forward()
+    motor_driver.right_forward(dc, freq)
     time.sleep(1.0)
     motor_driver.stop()
 
-    motor_driver.left_backward()
+    motor_driver.left_backward(dc, freq)
     time.sleep(1.0)
     motor_driver.stop()
 
-    motor_driver.right_backward()
+    motor_driver.right_backward(dc, freq)
     time.sleep(1.0)
     motor_driver.stop()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Test program for HY-SRF05 sensor.")
+    parser.add_argument(
+        '-d', '--pwm-dc', type=float, default=50.0,
+        help='Duty cycle percentage for PWM: [0.0, 100.0]')
+    parser.add_argument(
+        '-f', '--pwm-freq', type=float, default=10.0,
+        help='Frequency of PWM in Hz.')
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
